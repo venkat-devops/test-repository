c63eabd893ce9703d87c90f24120e1cde47930d7
Venkat Purushothama <venkat.purushothama@digital.dvla.gov.uk>
2015-03-07 22:35:03 +0000

    added dev branch & site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,7 +43,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include test
+  include test # This site.pp is from development branch
   #include profiles::notify_a_message
 }
 
