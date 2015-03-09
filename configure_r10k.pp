bc0f8d616b4f205da4cd5ba49d832b22772bd0e2
venkatp <venkat.devops@gmail.com>
2015-03-09 11:51:36 +0000

    Update configure_r10k.pp

--- configure_r10k.pp
+++ configure_r10k.pp
@@ -10,7 +10,7 @@ class { 'r10k':
   version           => '1.3.2',
   sources           => {
     'puppet' => {
-      'remote'  => 'https://github.com/glarizza/puppet_repository.git',
+      'remote'  => 'https://github.com/venkat-devops/test-repository.git',
       'basedir' => "${::settings::confdir}/environments",
       'prefix'  => false,
     }
